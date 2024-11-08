062fe1d7251fb23242d41a4a3ec655507dba2b0b
JaiDhiman <jaidhiman2000@gmail.com>
2024-11-07 18:32:03 -0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./Header";
+import { StoriesPage } from "./StoriesPage";
+import { Footer } from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <StoriesPage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/StoriesPage.jsx
@@ -0,0 +1,7 @@
+export function StoriesPage() {
+  return (
+    <main>
+      <h1>Welcome to React!</h1>
+    </main>
+  );
+}
